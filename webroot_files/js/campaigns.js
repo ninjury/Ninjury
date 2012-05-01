2ce8bda8161fc8b1f9abc5ce314a351bf46b6a29
Alex <alexander_kurihara@brown.edu>
2012-05-01 15:46:08 -0400

    still working ajax

--- webroot_files/js/campaigns.js
+++ webroot_files/js/campaigns.js
@@ -5,7 +5,7 @@ var ajax_load = "Loading";
 
 //  campaigns_sent
 function campaigns_sent(page){
-	var loadUrl = "index/campaigns/sent/" + page;  
+	var loadUrl = "index/ajax/campaigns/sent/" + page;  
 	alert(loadUrl);
     $("#campaigns_sent").html(ajax_load).load(loadUrl);  
 }
