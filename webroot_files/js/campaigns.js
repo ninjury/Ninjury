4742545783badc936e552b51a23b11a4b377684c
Alex <alexander_kurihara@brown.edu>
2012-05-01 15:53:15 -0400

    new stuff

--- webroot_files/js/campaigns.js
+++ webroot_files/js/campaigns.js
@@ -6,7 +6,10 @@ var ajax_load = "Loading";
 //  campaigns_sent
 function campaigns_sent(page){
 	var loadUrl = "index/ajax/campaigns/sent/" + page;  
-	alert(loadUrl);
-    $("#campaigns_sent").html(ajax_load).load(loadUrl);  
+	//alert(loadUrl);
+    //$("#campaigns_sent").html(ajax_load).load(loadUrl); 
+
+    $("#campaigns_sent").html(ajax_load);  
+        $.get(loadUrl, {language: "php", version: 5}, function(responseText){ $("#campaigns_sent").html(responseText); },"html");  
 }
 
